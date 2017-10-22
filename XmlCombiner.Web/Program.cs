5019d2806fe115b1e4633b53412a74a7b3986f6e
Shamus03 <shamus03@me.com>
2017-10-22 15:05:01 -0500

    no migrato

--- XmlCombiner.Web/Program.cs
+++ XmlCombiner.Web/Program.cs
@@ -15,35 +15,7 @@ namespace XmlCombiner.Web
     {
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
-
-            Migrate(host.Services);
-
-            host.Run();
-        }
-
-        public static void Migrate(IServiceProvider services)
-        {
-            bool migrated = false;
-            int attempts = 0;
-            while (!migrated)
-            {
-                try
-                {
-                    using (var scope = services.CreateScope())
-                    {
-                        var ctx = scope.ServiceProvider.GetRequiredService<XmlCombinerContext>();
-                        ctx.Database.EnsureCreated();
-                        ctx.Database.Migrate();
-                        migrated = true;
-                    }
-                }
-                catch (Exception) when (attempts < 5)
-                {
-                    Thread.Sleep(TimeSpan.FromSeconds(5));
-                    attempts += 1;
-                }
-            }
+            BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
